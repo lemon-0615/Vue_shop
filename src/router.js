a83764a55df1bb991fa47b95bba24d1bd11ca905
huang yanlin <2510555317@qq.com>
2021-05-13 21:46:02 +0800

    commit

---
+++ src/router.js
@@ -0,0 +1,10 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Login from './components/Login.vue'
+Vue.use(Router)
+export default new Router({
+  routes: [
+    { path: '/', redirect: Login },
+    { path: '/login', component: Login }
+  ]
+})
